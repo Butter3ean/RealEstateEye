2eaa353be2f495bac8aafe2f83803ca6d89bafba
Dalton Hughes <dhughes3223@gmail.com>
2023-05-12 00:49:57 -0400

    changed to use GlideImage to load images for MarkerInfoWindow in MapView.kt

--- app/src/main/java/com/example/realestateeye/views/MapView.kt
+++ app/src/main/java/com/example/realestateeye/views/MapView.kt
@@ -1,7 +1,6 @@
 package com.example.realestateeye.views
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,17 +10,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import coil.compose.AsyncImagePainter
-import coil.compose.rememberAsyncImagePainter
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 import com.example.realestateeye.models.RealEstateListing
 import com.example.realestateeye.viewmodels.RealEstateViewModel
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.*
-
 
 
 @SuppressLint("MissingPermission")
@@ -46,58 +45,64 @@ fun MapView() {
 }
 
 //@OptIn(ExperimentalGlideComposeApi::class)
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun MapMarkersWithCustomWindow(listings: List<RealEstateListing>) {
 
     for (listing in listings) {
 
-        val painter = rememberAsyncImagePainter(listing.urls.listingUrl)
-
-        if(painter.state is AsyncImagePainter.State.Success) {
-            MarkerInfoWindow(
-                state = MarkerState(
-                    position = LatLng(
-                        listing.coordinates.latitude,
-                        listing.coordinates.longitude
-                    )
-                ),
-                icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE),
+        MarkerInfoWindow(
+            state = MarkerState(
+                position = LatLng(
+                    listing.coordinates.latitude,
+                    listing.coordinates.longitude
+                )
+            ),
+            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE),
+        ) {
+            Column(
+                modifier = Modifier
+                    .size(350.dp)
+                    .background(
+                        Color.White,
+                        shape = RoundedCornerShape(35.dp, 35.dp, 35.dp, 35.dp)
+                    ),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Column(
+
+                Text(text = listing.mlsNum.toString(), modifier = Modifier.padding(8.dp))
+
+                GlideImage(
+                    model = listing.urls.imageUrl,
+                    contentDescription = null,
                     modifier = Modifier
-                        .size(450.dp)
-                        .background(
-                            Color.White,
-                            shape = RoundedCornerShape(35.dp, 35.dp, 35.dp, 35.dp)
-                        )
-                ) {
+                        .padding(12.dp)
+                        .clip(RoundedCornerShape(25.dp))
+                )
 
-                    Image(painter = painter, contentDescription = null)
-
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.Center
-                        ) {
-                            Text(text = "Beds: " + listing.details.beds)
-                            Spacer(modifier = Modifier.padding(8.dp))
-                            Text(text = "Baths: " + listing.details.baths)
-                        }
-
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Text(text = "Beds: " + listing.details.beds)
                         Spacer(modifier = Modifier.padding(8.dp))
-
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.Center
-                        ) {
-                            Text(text = "Price: " + listing.details.price)
-                            Spacer(modifier = Modifier.padding(8.dp))
-                            Text(text = "SqFt: " + listing.details.sqFt)
-                        }
-
-                        Spacer(modifier = Modifier.padding(16.dp))
-
+                        Text(text = "Baths: " + listing.details.baths)
                     }
+
+                    Spacer(modifier = Modifier.padding(8.dp))
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Text(text = "Price: " + listing.details.price)
+                        Spacer(modifier = Modifier.padding(8.dp))
+                        Text(text = "SqFt: " + listing.details.sqFt)
+                    }
+
+                    Spacer(modifier = Modifier.padding(16.dp))
+
                 }
             }
         }
